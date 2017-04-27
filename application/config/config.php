fc073293520648ee90661b436a3fb9ff51af8308
Hirwa Jean Luc <hirwajluc@gmail.com>
2017-04-27 20:43:30 +0200

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/chris/';
+$config['base_url'] = 'https://ishema.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
